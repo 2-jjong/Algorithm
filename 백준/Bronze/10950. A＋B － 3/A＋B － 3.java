b94ef53d08a7be94eebe943cf620bbe42ec4ad55
2-jjong <jhyeon2450@gmail.com>
2024-11-03 19:26:39 +0900

    [Bronze V] Title: A+B - 3, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+			sb.append(num1 + num2).append("\n");
+		}
+
+		System.out.println(sb);
+
+	}
+
+}
