30ed70a9ae0cce4ac908360551a315e23e1106a5
2-jjong <jhyeon2450@gmail.com>
2024-11-10 22:06:12 +0900

    [D1] Title: 신문 헤드라인, Time: 98 ms, Memory: 18,344 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+//		System.setIn(new FileInputStream("C:\\Users\\user\\Downloads\\input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		
+		System.out.println(str.toUpperCase());
+
+	}
+
+}
