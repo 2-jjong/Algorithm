67663cb0e2ae681fda733f5153038269f97ae460
2-jjong <jhyeon2450@gmail.com>
2025-02-17 16:36:54 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 123 ms, Memory: 20,236 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,49 +1,81 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Solution {
 
-	public static void main(String[] args) throws IOException {
-//		System.setIn(new FileInputStream("C:\\Users\\user\\Downloads\\input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		/**
+		 * 0. 입력파일 읽어들이기
+		 */
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		// 결과를 한 번에 출력하기 위한 StringBuilder
 		StringBuilder sb = new StringBuilder();
 
-		for (int testCase = 1; testCase <= 10; testCase++) {
-			int n = Integer.parseInt(br.readLine());
-			int[] box = new int[100];
-			PriorityQueue<Integer> pq = new PriorityQueue<>();
-			StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = 10;
+		for (int test_case = 1; test_case <= T; test_case++) {
+			sb.append("#" + test_case + " ");
 
-			for (int i = 0; i < 100; i++) {
-				box[i] = Integer.parseInt(st.nextToken());
+			// 알고리즘 코드 작성하기
+			/**
+			 * 1. 입력 파일 객체화
+			 */
+			int dumpCount = Integer.parseInt(in.readLine());
+			String[] tempArr = in.readLine().split(" ");
+			int[] boxes = new int[100];
+
+			for (int i = 0; i < boxes.length; i++) {
+				boxes[i] = Integer.parseInt(tempArr[i]);
 			}
 
-			for (int i = 0; i < n; i++) {
-				dump(box);
+			/**
+			 * 2. 알고리즘 풀기
+			 */
+
+			int result = -1;
+
+			for (int i = 0; i < dumpCount; i++) {
+				int minIndex = 0;
+				int maxIndex = 0;
+
+				for (int j = 1; j < boxes.length; j++) {
+					if (boxes[minIndex] > boxes[j])
+						minIndex = j;
+
+					if (boxes[maxIndex] < boxes[j])
+						maxIndex = j;
+				}
+
+				// 가장 큰 높이의 작은 높이의 차이가 0 또는 1이면 break;
+				if (boxes[maxIndex] - boxes[minIndex] <= 1) {
+					result = boxes[maxIndex] - boxes[minIndex];
+					break;
+				}
+
+				// dump
+				boxes[minIndex]++;
+				boxes[maxIndex]--;
 			}
-			Arrays.sort(box);
-			int answer = box[99] - box[0];
+			
+			int min = boxes[0];
+			int max = boxes[0];
+			for (int i = 1; i < boxes.length; i++) {
+			    if (min > boxes[i]) min = boxes[i];
+			    if (max < boxes[i]) max = boxes[i];
+			}
+			result = max - min;
 
-			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
+			/**
+			 * 3. 정답 출력
+			 */
+			sb.append(result).append("\n");
 		}
 
-		System.out.print(sb);
-	}
+		System.out.println(sb);
 
-	public static void dump(int[] box) {
-		int minIndex = 0;
-		int maxIndex = 0;
-
-		for (int i = 1; i < box.length; i++) {
-			if (box[minIndex] > box[i])
-				minIndex = i;
-
-			if (box[maxIndex] < box[i])
-				maxIndex = i;
-		}
-
-		box[minIndex]++;
-		box[maxIndex]--;
 	}
 
 }
