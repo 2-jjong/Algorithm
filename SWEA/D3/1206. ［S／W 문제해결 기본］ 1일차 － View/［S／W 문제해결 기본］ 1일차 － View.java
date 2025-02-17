24f2287b25e1e4ec16854c5b806aa4c565e44456
2-jjong <jhyeon2450@gmail.com>
2025-02-17 15:35:02 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 125 ms, Memory: 23,584 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -1,43 +1,59 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
 
 public class Solution {
 
-	public static void main(String[] args) throws IOException {
-//		System.setIn(new FileInputStream("C:\\Users\\user\\Downloads\\sample_input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		/**
+		 * 0. 입력파일 읽어들이기
+		 */
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		// 결과를 한 번에 출력하기 위한 StringBuilder
 		StringBuilder sb = new StringBuilder();
 
-		for (int testCase = 1; testCase <= 10; testCase++) {
-			int n = Integer.parseInt(br.readLine());
-			int[] arr = new int[n];
-			StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = 10;
+		for (int test_case = 1; test_case <= T; test_case++) {
+			sb.append("#" + test_case + " ");
 
-			for (int i = 0; i < n; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
-			}
+			// 알고리즘 코드 작성하기
+			/**
+			 * 1. 입력 파일 객체화
+			 */
+			in.readLine();
+			String[] buildings = in.readLine().split(" ");
 
-			int answer = 0;
+			/**
+			 * 2. 알고리즘 풀기
+			 */
+			int result = 0;
 
-			for (int i = 2; i < n - 2; i++) {
-				int currentMax = Integer.MIN_VALUE;
-				int currentHeight = arr[i];
+			for (int i = 2; i < buildings.length - 2; i++) {
+				int height = Integer.parseInt(buildings[i]);
+				int max = Integer.MIN_VALUE;
 
 				for (int j = i - 2; j <= i + 2; j++) {
-					if (j == i)
-						continue;
-
-					currentMax = Math.max(currentMax, arr[j]);
+					if (j != i) {
+						int temp = Integer.parseInt(buildings[j]);
+						max = Math.max(max, temp);
+					}
 				}
 
-				if (currentMax < currentHeight)
-					answer += currentHeight - currentMax;
-
+				if (height > max) {
+					result += height - max;
+				}
 			}
 
-			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
+			/**
+			 * 3. 정답 출력
+			 */
+			sb.append(result).append("\n");
 		}
 
-		System.out.print(sb);
+		System.out.println(sb);
+
 	}
+
 }
