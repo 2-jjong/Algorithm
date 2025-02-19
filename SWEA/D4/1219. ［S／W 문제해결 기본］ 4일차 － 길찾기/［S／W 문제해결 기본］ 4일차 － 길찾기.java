7540d9ba75d54a57c84e4be76e855151447b3f87
2-jjong <jhyeon2450@gmail.com>
2025-02-19 15:19:52 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 길찾기, Time: 120 ms, Memory: 20,644 KB -BaekjoonHub

--- SWEA/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/［S／W 문제해결 기본］ 4일차 － 길찾기.java
+++ SWEA/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/［S／W 문제해결 기본］ 4일차 － 길찾기.java
@@ -1,60 +1,81 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Solution {
 
-	public static void main(String[] args) throws IOException {
-//		System.setIn(new FileInputStream("C:\\Users\\user\\Downloads\\input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	// DFS
+	public static void dfs(int node, List<Integer>[] graph, boolean[] visited) {
+
+		// 현재 노드 방문 처리 및 출력
+		visited[node] = true;
+
+		// 인접 노드들 방문 (재귀 호출)
+		for (int neighbor : graph[node]) {
+			if (!visited[neighbor]) { // 이미 방문한 노드는 탐색 안 함
+				dfs(neighbor, graph, visited);
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		/**
+		 * 0. 입력파일 읽어들이기
+		 */
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		// 결과를 한 번에 출력하기 위한 StringBuilder
 		StringBuilder sb = new StringBuilder();
 
 		for (int testCase = 1; testCase <= 10; testCase++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			st.nextToken();
-			int e = Integer.parseInt(st.nextToken());
-			List<Integer>[] graph = new ArrayList[100];
-			boolean[] visited = new boolean[100];
-			for (int i = 0; i < 100; i++) {
+			sb.append("#" + testCase + " ");
+
+			// 알고리즘 코드 작성하기
+			/**
+			 * 1. 입력 파일 객체화
+			 */
+
+			int n = 100; // 노드 개수 (0번부터 99번 노드 사용)
+			List<Integer>[] graph = new ArrayList[n];
+
+			// 인접 리스트 초기화
+			for (int i = 0; i < n; i++) {
 				graph[i] = new ArrayList<>();
 			}
 
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < e; i++) {
+			// 그래프 간선 추가
+			in.readLine();
+			StringTokenizer st = new StringTokenizer(in.readLine());
+
+			while (st.hasMoreTokens()) {
 				int start = Integer.parseInt(st.nextToken());
 				int end = Integer.parseInt(st.nextToken());
 
 				graph[start].add(end);
 			}
 
-			bfs(graph, visited, 0);
+			// 방문 여부 추적 배열
+			boolean[] visited = new boolean[n];
 
+			/**
+			 * 2. 알고리즘 풀기
+			 */
+
+			// DFS 탐색 시작
+			dfs(0, graph, visited);
+
+			/**
+			 * 3. 정답 출력
+			 */
 			int answer = visited[99] ? 1 : 0;
 
-			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
+			sb.append(answer).append("\n");
 
 		}
 
 		System.out.println(sb);
-
 	}
-
-	public static void bfs(List<Integer>[] graph, boolean[] visited, int start) {
-		Queue<Integer> queue = new LinkedList<>();
-
-		queue.add(start);
-		visited[start] = true;
-
-		while (!queue.isEmpty()) {
-			int node = queue.poll();
-
-			for (int next : graph[node]) {
-				if (!visited[next]) {
-					queue.add(next);
-					visited[next] = true;
-				}
-			}
-		}
-
-	}
-
 }
