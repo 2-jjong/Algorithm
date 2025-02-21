5fea86877470c05fe41aab65ff6fc12831d9f8f8
2-jjong <jhyeon2450@gmail.com>
2025-02-21 16:52:29 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 140 ms, Memory: 20,152 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
@@ -1,69 +1,96 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Solution {
+	private static List<Integer>[] graph;
+	private static boolean[] visited;
 
-	public static void main(String[] args) throws IOException {
-//		System.setIn(new FileInputStream("C:\\Users\\user\\Downloads\\input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static int bfs(int vertex, int depth) {
+		Queue<int[]> queue = new LinkedList<>();
+		int answerVertex = vertex;
+		int maxDepth = depth;
+
+		visited[vertex] = true;
+		queue.add(new int[] { vertex, depth });
+
+		while (!queue.isEmpty()) {
+			int[] currentNode = queue.poll();
+			int currentVertex = currentNode[0];
+			int currentDepth = currentNode[1];
+
+			if (currentDepth > maxDepth) {
+				maxDepth = currentDepth;
+				answerVertex = currentVertex;
+			} else if (currentDepth == maxDepth) {
+				answerVertex = Math.max(answerVertex, currentVertex);
+			}
+
+			for (int next : graph[currentVertex]) {
+				if (!visited[next]) {
+					visited[next] = true;
+					queue.add(new int[] { next, currentDepth + 1 });
+				}
+			}
+		}
+
+		return answerVertex;
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		/**
+		 * 0. 입력파일 읽어들이기
+		 */
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
-		for (int testCase = 1; testCase <= 10; testCase++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			int start = Integer.parseInt(st.nextToken());
-			List<Integer>[] graph = new ArrayList[101];
-			boolean[] visited = new boolean[101];
-			int[] distances = new int[101];
-			Arrays.fill(distances, 0);
+		int T = 10;
+		for (int test_case = 1; test_case <= T; test_case++) {
+			sb.append("#" + test_case + " ");
 
-			for (int i = 1; i <= 100; i++) {
+			/**
+			 * 1. 입력 파일 객체화
+			 */
+
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int start = Integer.parseInt(st.nextToken());
+
+			graph = new ArrayList[101];
+			visited = new boolean[101];
+
+			for (int i = 1; i < graph.length; i++) {
 				graph[i] = new ArrayList<>();
 			}
 
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < n / 2; i++) {
+			st = new StringTokenizer(in.readLine());
+			for (int i = 0; i < N / 2; i++) {
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
 
 				graph[from].add(to);
 			}
 
-			bfs(graph, visited, distances, start);
+			/**
+			 * 2. 알고리즘 풀기
+			 */
 
-			int maxIndex = 0;
+			int answer = bfs(start, 0);
 
-			for (int i = 1; i <= 100; i++) {
-				if (distances[i] >= distances[maxIndex]) {
-					maxIndex = i;
-				}
-			}
+			/**
+			 * 3. 정답 출력
+			 */
 
-			sb.append("#").append(testCase).append(" ").append(maxIndex).append("\n");
-
+			sb.append(answer).append("\n");
 		}
 
 		System.out.println(sb);
-
 	}
-
-	public static void bfs(List<Integer>[] graph, boolean[] visited, int[] distances, int start) {
-		Queue<Integer> queue = new LinkedList<>();
-
-		distances[start] = 1;
-		queue.add(start);
-
-		while (!queue.isEmpty()) {
-			int node = queue.poll();
-
-			for (int next : graph[node]) {
-				if (!visited[next]) {
-					queue.add(next);
-					distances[next] = distances[node] + 1;
-					visited[next] = true;
-				}
-			}
-		}
-	}
-
 }
