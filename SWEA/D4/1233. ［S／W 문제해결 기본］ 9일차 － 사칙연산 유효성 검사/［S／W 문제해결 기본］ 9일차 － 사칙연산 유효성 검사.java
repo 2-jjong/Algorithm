cea372b3b405f62bb209258a9629035e86c6f0aa
2-jjong <jhyeon2450@gmail.com>
2025-02-25 15:06:13 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 121 ms, Memory: 20,932 KB -BaekjoonHub

--- SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -1,69 +1,93 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-	static class Node {
-		String value;
-		int left;
-		int right;
+	private static int N;
+	private static Node[] tree;
 
-		public Node(String value, int left, int right) {
-			this.value = value;
-			this.left = left;
-			this.right = right;
+	public static boolean check(Node node) {
+		if (node == null) {
+			return true;
 		}
 
+		boolean left = check(node.left);
+		boolean right = check(node.right);
+		
+	    String value = node.value;
+
+	    if (value.equals("+") || value.equals("-") || value.equals("*") || value.equals("/")) {
+	        return node.left != null && node.right != null && left && right;
+	    } else {
+	        return node.left == null && node.right == null;
+	    }
 	}
 
-	static Node[] tree;
-	static StringBuilder sb;
+	public static void main(String[] args) throws Exception {
 
-	public static void main(String[] args) throws IOException {
-//		System.setIn(new FileInputStream("C:\\Users\\user\\Downloads\\input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
+		/**
+		 * 0. 입력파일 읽어들이기
+		 */
 
-		for (int testCase = 1; testCase <= 10; testCase++) {
-			int n = Integer.parseInt(br.readLine());
-			tree = new Node[n + 1];
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-			for (int i = 1; i <= n; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				int vertex = Integer.parseInt(st.nextToken());
-				String value = st.nextToken();
-				int left = st.hasMoreTokens() ? Integer.parseInt(st.nextToken()) : -1;
-				int right = st.hasMoreTokens() ? Integer.parseInt(st.nextToken()) : -1;
+		int T = 10;
+		for (int test_case = 1; test_case <= T; test_case++) {
+			sb.append("#" + test_case + " ");
 
-				tree[i] = new Node(value, left, right);
+			/**
+			 * 1. 입력 파일 객체화
+			 */
+
+			N = Integer.parseInt(in.readLine());
+			tree = new Node[N + 1];
+
+			for (int i = 1; i < N + 1; i++) {
+				tree[i] = new Node();
 			}
 
-			boolean result = preOrder(1);
+			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(in.readLine());
+
+				int index = Integer.parseInt(st.nextToken());
+
+				tree[index].value = st.nextToken();
+
+				if (st.hasMoreTokens()) {
+					int leftIndex = Integer.parseInt(st.nextToken());
+					tree[index].left = tree[leftIndex];
+				}
+
+				if (st.hasMoreTokens()) {
+					int rightIndex = Integer.parseInt(st.nextToken());
+					tree[index].right = tree[rightIndex];
+				}
+			}
+
+			/**
+			 * 2. 알고리즘 풀기
+			 */
+
+			boolean result = check(tree[1]);
+
+			/**
+			 * 3. 정답 출력
+			 */
 
 			int answer = result ? 1 : 0;
 
-			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
+			sb.append(answer).append("\n");
 
 		}
 
 		System.out.println(sb);
-
 	}
+}
 
-	public static boolean preOrder(int vertex) {
-		Node node = tree[vertex];
-
-		if ((node.value.equals("+") || node.value.equals("-") || node.value.equals("*") || node.value.equals("/"))
-				&& (node.left != -1 && node.right != -1)) {
-			if (preOrder(node.left) && preOrder(node.right)) {
-				return true;
-			}
-		} else {
-			if (node.left == -1 && node.right == -1) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
+class Node {
+	String value;
+	Node left;
+	Node right;
 }
