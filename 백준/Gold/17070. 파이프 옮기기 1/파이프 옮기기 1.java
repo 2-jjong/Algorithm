913295bd4a574b6fd30ba875539ba804c1ae1a7b
2-jjong <jhyeon2450@gmail.com>
2025-03-18 14:41:01 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 108 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[][] map = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		// 0은 가로 1은 세로 2는 대각선
+		int[][][] dp = new int[N][N][3];
+
+		// 초기 파이프는 가로
+		dp[0][1][0] = 1;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				// 초기 파이프 건너뛰기
+				if (i == 0 && j == 1)
+					continue;
+
+				// 벽이면 건너뛰기
+				if (map[i][j] == 1)
+					continue;
+
+				if (j - 1 >= 0)
+					dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+				if (i - 1 >= 0)
+					dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+				if (i - 1 >= 0 && j - 1 >= 0 && map[i-1][j] != 1 && map[i][j-1] != 1)
+					dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+			}
+		}
+
+		int answer = dp[N - 1][N - 1][0] + dp[N - 1][N - 1][1] + dp[N - 1][N - 1][2];
+
+		System.out.println(answer);
+
+	}
+}
