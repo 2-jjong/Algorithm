8389311dc9ad97edec0ddfecf99cbc9a24070fee
2-jjong <jhyeon2450@gmail.com>
2026-03-07 12:54:48 +0900

    [Silver V] Title: 덩치, Time: 64 ms, Memory: 11596 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        int[][] people = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            people[i][0] = Integer.parseInt(st.nextToken()); // 몸무게
+            people[i][1] = Integer.parseInt(st.nextToken()); // 키
+        }
+
+        // 각 사람마다 전체를 돌며 나보다 덩치 큰 사람 카운트
+        for (int i = 0; i < N; i++) {
+            int rank = 1;
+            
+            for (int j = 0; j < N; j++) {
+                if (i == j)
+                    continue;
+
+                if (people[j][0] > people[i][0] && people[j][1] > people[i][1]) {
+                    rank++;
+                }
+            }
+            sb.append(rank).append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+}
