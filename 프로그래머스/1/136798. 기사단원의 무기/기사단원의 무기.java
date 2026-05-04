60b17e7227de0e914d72f43a184450b4ba9cbff9
2-jjong <jhyeon2450@gmail.com>
2026-05-04 21:21:35 +0900

    [level 1] Title: 기사단원의 무기, Time: 71.01 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int totalWeight = 0;
+
+        for (int i = 1; i <= number; i++) {
+            int divisors = getDivisorCount(i);
+            
+            if (divisors > limit) {
+                totalWeight += power;
+            } else {
+                totalWeight += divisors;
+            }
+        }
+        
+        return totalWeight;
+    }
+
+    private int getDivisorCount(int n) {
+        int count = 0;
+        
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (i * i == n) {
+                    count += 1;
+                } else {
+                    count += 2;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
