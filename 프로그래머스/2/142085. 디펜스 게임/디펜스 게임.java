4a3c9ae3d514cef22e6918a21e2571c80399934a
2-jjong <jhyeon2450@gmail.com>
2026-06-26 20:54:42 +0900

    [level 2] Title: 디펜스 게임, Time: 64.76 ms, Memory: 152 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int i = 0; i < enemy.length; i++) {
+            int currentEnemy = enemy[i];
+            
+            n -= currentEnemy;
+            pq.add(currentEnemy);
+            
+            if (n < 0) {
+                if (k > 0) {
+                    n += pq.poll();
+                    k--;
+                } else {
+                    break;
+                }
+            }
+            
+            answer++;
+        }
+        
+        return answer;
+    }
+}
